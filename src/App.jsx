324fa4cc0655b2fcf70f4d6872b91d6603a7c87b
JuCalll <123980459+JuCalll@users.noreply.github.com>
2023-07-25 16:55:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import IMG from "./components/img"
+
+function Imagenes() {
+  
+  return (
+    <>
+    <div>
+      <img src="../public/IMG/Imagen de WhatsApp 2023-07-25 a las 15.25.09.jpg" className="img1" />
+      <br />
+      <img src="../public/IMG/IMGADSO.jpg" className="img2" />
+    </div>
+    <div class="patterns pt1"></div>
+
+    <br />
+    <br />
+
+    <textarea
+      rows="5"
+      cols="50"
+      placeholder="Texto..."
+    />
+    </>
+  )
+}
+
+export default Imagenes
---
+++ src/components/img.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './input.css'
+
+
+function IMG ({img}) {
+    return (
+        <div>
+            <img img = {img}/>
+        </div>
+    )
+}
+
+export default IMG
